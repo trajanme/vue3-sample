30e8b67ea038cb81e8f3d8fd0b2312894000e68e
trajanme <shintaro@trajan.me>
2022-12-11 17:04:39 +0900

    add moment

--- src/components/MomentSample.vue
+++ src/components/MomentSample.vue
@@ -1,6 +1,20 @@
+<script>
+import moment from "moment"
+
+export default {
+  name: "MomentSample",
+  computed: {
+    getTime() {
+      return moment().format("YYYY-MM-DD HH:mm:ss");
+    }
+  }
+}
+</script>
+
 <template>
   <div class="momentSample">
     <h1>Moment Sample</h1>
     <h3>Current Time</h3>
+    <p>{{ getTime }}</p>
   </div>
 </template>
